a70d50c9a80807ca32be81f1b008f6fffae38906
Tyler Thomas <alogia@gmail.com>
2017-01-30 22:42:57 -0600

    foo

--- js/black.album.js
+++ js/black.album.js
@@ -9,21 +9,6 @@ function applyTiles() {
 
 jQuery(document).ready(function($) {
 
-	var siteUrl = 'http://'+(document.location.hostname||document.location.host);
-
-	document.title = $(data).find("title").text();
-	$(document).delegate('a[href^="/"],a[href^="'+siteUrl+'"]', "click", function(e) {
-		e.preventDefault();
-		History.pushState({}, "", this.pathname);
-	});
-	History.Adapter.bind(window, 'statechange', function(){
-		var State = History.getState();
-		$.get(State.url, function(data){
-			document.title = $(data).find("title").text();
-			$('.content').html($(data).find('.content'));
-			_gaq.push(['_trackPageview', State.url]);
-		});
-	});
 
 	$(function() {
 		applyTiles();
